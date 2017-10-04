cb3d69421b3f6835ee59111c3d94c111b88f723f
Ariane Boissonnas <ariane@boissonnas.net>
2017-10-04 14:26:13 -0500

    added square function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
+#include <iostream>
+
 using namespace std;
+
+void square(int num);
 
 int main()
 {
 	
 
 	return 0;
+}
+
+//takes a number to be squared and prints its square
+void square(int num)
+{
+	cout << "The square of " << num << " is " << num*num << endl << endl;
 }
